e813e1299c7028ede6c5d268727fc6db513f6299
Filipasek <filipasek10@gmail.com>
2020-07-21 22:30:39 +0200

    fixed no-config issue

--- lib/tools/config.dart
+++ lib/tools/config.dart
@@ -7,20 +7,39 @@ class ConfigData extends ChangeNotifier {
   bool weatherLight;
   SharedPreferences prefs;
   List<String> templateConfig = ['true', '0'];
+
   /// 0 is white theme, 1 is dark theme
   int themeColor;
 
   void changeWeatherLightBoolean(newValue) {
-    weatherLight = newValue;
-    List<String> oldValue = prefs.getStringList('configData') ?? templateConfig;
-    oldValue[0] = newValue.toString();
-    prefs.setStringList('configData', oldValue);
+    saveConfig(id: 0, newValue: newValue);
     notifyListeners();
+  }
+
+  void saveConfig({@required id, @required newValue}) {
+    List<String> oldValue = prefs.getStringList('configData') ?? templateConfig;
+    oldValue[id] = newValue.toString();
+    prefs.setStringList('configData', oldValue);
   }
 
   void readConfigs() async {
     prefs = await SharedPreferences.getInstance();
-    List<String> configData = prefs.getStringList('configData') ?? templateConfig;
+    List<String> notPresent() {
+      prefs.setStringList('configData', templateConfig);
+      return templateConfig;
+    }
+
+    List<String> configData = prefs.getStringList('configData') ?? notPresent();
+    // throw Exception();
+    if (configData.length < templateConfig.length) {
+      List<String> newArray = templateConfig;
+
+      for (var i = 0; i <= configData.length - 1; i++) {
+        newArray[i] = configData[i];
+      }
+      prefs.setStringList('configData', newArray);
+      configData = newArray;
+    }
     weatherLight = configData[0] == 'false' ? false : true;
     themeColor = int.parse(configData[1] ?? '0');
     notifyListeners();
